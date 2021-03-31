e15d9836f806ddb8a8f017d0a661ba5d98bd38c5
sherwinski <sherwin@imgix.com>
2021-03-31 17:29:40 -0400

    feat: create ImgixAPI instance with user-supplied API key

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -1,13 +1,37 @@
-import React from 'react';
-import { Paragraph } from '@contentful/forma-36-react-components';
+import React, { Component } from 'react';
+import { Button, Paragraph } from '@contentful/forma-36-react-components';
 import { DialogExtensionSDK } from 'contentful-ui-extensions-sdk';
+// @ts-ignore - TODO add .d.ts file
+import ImgixApi from 'imgix-management-js';
 
 interface DialogProps {
   sdk: DialogExtensionSDK;
 }
 
-const Dialog = (props: DialogProps) => {
-  return <Paragraph>Hello Dialog Component</Paragraph>;
-};
+interface DialogState {
+  imgix: any; // TODO - replace with class type
+}
 
-export default Dialog;
+export default class Dialog extends Component<DialogProps, DialogState> {
+  constructor(props: DialogProps) {
+    super(props);
+
+    const apiKey = (props as any).sdk.parameters.installation.imgixAPIKey;
+    const imgix = new ImgixApi({
+      apiKey,
+    });
+
+    this.state = {
+      imgix,
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <Paragraph>Hello Dialog Component</Paragraph>
+        <Button onClick={() => this.props.sdk.close('Done!')} />
+      </div>
+    );
+  }
+}
