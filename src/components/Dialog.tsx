3956628a6509abd6cd47c5a0ad659650882b7dc5
sherwinski <sherwin@imgix.com>
2021-04-13 13:49:57 -0400

    feat: import ImgixAPI type definitions

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -7,15 +7,14 @@ import {
   DropdownListItem,
 } from '@contentful/forma-36-react-components';
 import { DialogExtensionSDK } from 'contentful-ui-extensions-sdk';
-// @ts-ignore - TODO add .d.ts file
-import ImgixApi from 'imgix-management-js';
+import ImgixAPI from 'imgix-management-js';
 
 interface DialogProps {
   sdk: DialogExtensionSDK;
 }
 
 interface DialogState {
-  imgix: any; // TODO - replace with class type
+  imgix: ImgixAPI;
   isOpen: boolean;
   allSources: Array<SourceProps>;
   selectedSource: Partial<SourceProps>;
@@ -32,7 +31,7 @@ export default class Dialog extends Component<DialogProps, DialogState> {
 
     const apiKey =
       (props as any).sdk?.parameters.installation.imgixAPIKey || '';
-    const imgix = new ImgixApi({
+    const imgix = new ImgixAPI({
       apiKey,
     });
 
