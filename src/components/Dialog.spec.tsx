a194724325d84a566314c943093c3524b7eb93b9
sherwinski <sherwin@imgix.com>
2021-05-10 11:53:14 -0400

    test: pass in mocked API key via component props

--- src/components/Dialog.spec.tsx
+++ src/components/Dialog.spec.tsx
@@ -4,8 +4,15 @@ import { render } from '@testing-library/react';
 
 describe('Dialog component', () => {
   it('Component text exists', () => {
-    const { getByText } = render(<Dialog />);
+    const mockSdk: any = {
+        parameters: {
+          installation: {
+            imgixAPIKey: 'test_key_123'
+          }
+        }
+    };
 
+    const { getByText } = render(<Dialog sdk={mockSdk}/>);
     expect(getByText('Hello Dialog Component')).toBeInTheDocument();
   });
 });
