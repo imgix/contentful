a14fadea6cd1c9d97da1d72e93d910e205c42798
sherwinski <sherwin@imgix.com>
2021-05-11 17:40:14 -0400

    style: lint Dialog.spec.tsx

--- src/components/Dialog.spec.tsx
+++ src/components/Dialog.spec.tsx
@@ -5,14 +5,14 @@ import { render } from '@testing-library/react';
 describe('Dialog component', () => {
   it('Component text exists', () => {
     const mockSdk: any = {
-        parameters: {
-          installation: {
-            imgixAPIKey: 'test_key_123'
-          }
-        }
+      parameters: {
+        installation: {
+          imgixAPIKey: 'test_key_123',
+        },
+      },
     };
 
-    const { getByText } = render(<Dialog sdk={mockSdk}/>);
+    const { getByText } = render(<Dialog sdk={mockSdk} />);
     expect(getByText('Hello Dialog Component')).toBeInTheDocument();
   });
 });
