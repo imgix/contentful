1c4f37bbcb009ee215761314d5f72d570cd8db35
Luis Ball <lball@imgix.com>
2021-07-14 10:41:28 -0500

    refactor: create image grid component
    This commit refactors the image grid into its own component for easier fine tuned control over the gird

---
+++ src/components/Gallery/ImageGrid.tsx
@@ -0,0 +1,35 @@
+import { FunctionComponent } from 'react';
+import Imgix from 'react-imgix';
+
+interface ImageGridProps {
+  images: string[];
+  onClick: (event: any) => void;
+}
+
+export const ImageGrid: FunctionComponent<ImageGridProps> = ({
+  images,
+  onClick,
+}) => {
+  return (
+    <>
+      {images.map((url: string) => (
+        <div className="ix-gallery-item">
+          <Imgix
+            src={url}
+            width={140}
+            height={140}
+            imgixParams={{
+              auto: 'format',
+              fit: 'crop',
+              crop: 'entropy',
+            }}
+            sizes="(min-width: 480px) calc(12.5vw - 20px)"
+            htmlAttributes={{
+              onClick: () => onClick(url),
+            }}
+          />
+        </div>
+      ))}
+    </>
+  );
+};
