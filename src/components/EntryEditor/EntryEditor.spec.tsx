91723adc3a07dc4639e7d42a72049ce6b8e30cbc
Luis Ball <lball@imgix.com>
2021-07-14 16:35:25 -0500

    chore: move entry editor into its own directory

--- src/components/EntryEditor/EntryEditor.spec.tsx
+++ src/components/EntryEditor/EntryEditor.spec.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import EntryEditor from './EntryEditor';
+import EntryEditor from './';
 import { render } from '@testing-library/react';
 
 describe('Entry component', () => {
