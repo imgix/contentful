f17dfa08d9e7accca8e8c1fa2456c253919bc1c1
sherwinski <sherwin@imgix.com>
2021-07-15 01:30:55 -0700

    feat(dialog): add/export PageProps type
    The page props will keep track of pagination information:
    1) What is the user's current page index
    2) How many pages are there available in total

--- src/components/Dialog/index.ts
+++ src/components/Dialog/index.ts
@@ -1,6 +1,7 @@
-import Dialog, { SourceProps as _SourceProps } from './Dialog';
+import Dialog, { SourceProps as _SourceProps, PageProps as _PageProps } from './Dialog';
 import { DialogHeader as _DialogHeader } from './DialogHeader';
 
 export default Dialog;
 export const DialogHeader = _DialogHeader;
 export type SourceProps = _SourceProps;
+export type PageProps = _PageProps;
