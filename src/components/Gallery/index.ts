a3f65cffe48040de0dc27b54255800a437bc89ee
sherwinski <sherwin@imgix.com>
2021-07-15 01:22:09 -0700

    refactor(gallery): integrate the Pagination component into the Gallery component
    Before this change, the pagination buttons would render before the Gallery component fully renders (due to async API calls), causing a strange user experience. More specifically, the pagination dropdown would always initially render with the label "Page 1 of 1" but would quickly re-render once the async calls completed.
    Now that the pagination is integrated, it will not render until the API call is resolved which results in the initial render reflecting the correct pagination state.

--- src/components/Gallery/index.ts
+++ src/components/Gallery/index.ts
@@ -1,7 +1,9 @@
 import { Gallery as _ImageGallery } from './ImageGallery';
 import { ImageGrid as _ImageGrid } from './ImageGrid';
 import { ImagePlaceholder as _ImagePlaceholder } from './ImagePlaceholder';
+import { ImagePagination as _ImagePagination } from './ImagePagination';
 
 export const ImageGallery = _ImageGallery;
 export const ImageGrid = _ImageGrid;
 export const ImagePlaceholder = _ImagePlaceholder;
+export const ImagePagination = _ImagePagination;
