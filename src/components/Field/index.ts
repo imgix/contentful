939fff18649d58d99cb74896c44e77a9c1eb6c8c
sherwinski <sherwin@imgix.com>
2021-07-19 15:39:39 -0700

    feat: export FieldPrompt and FieldImagePreview components

--- src/components/Field/index.ts
+++ src/components/Field/index.ts
@@ -1,2 +1,7 @@
 import Field from './Field';
+import { FieldImagePreview as _FieldImagePreview} from './FieldImagePreview';
+import { FieldPrompt as _FieldPrompt } from './FieldPrompt';
+
 export default Field;
+export const FieldImagePreview = _FieldImagePreview;
+export const FieldPrompt = _FieldPrompt;
