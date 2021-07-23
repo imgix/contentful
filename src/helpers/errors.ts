0e0dbc11c072414498467d53a63f2e40858b7dc2
Luis Ball <lball@imgix.com>
2021-07-23 10:36:44 -0400

    chore(errors): create errors helper file
    This commit creates the `errors.ts` file, where a custom IxError error
    class is deinfed. The class accepts an error `type` of
    `InvalidApiKeyError` | `NoSourcesError` | `NoOriginImagesError`. Those
    types, in turn, have different `message`, `name`, and `type` values.
    Their `type` values corresponding to the type of `Note` they belong to.
    Ie, `InvalidApiKeyError` has a type of `negative`, or a red colored, Note.

---
+++ src/helpers/errors.ts
@@ -0,0 +1,80 @@
+export type NoteType = 'primary' | 'positive' | 'negative' | 'warning';
+export type ErrorType =
+  | `InvalidApiKeyError`
+  | `NoSourcesError`
+  | `NoOriginImagesError`;
+
+const DASHBOARD_URL = 'https://dashboard.imgix.com';
+const APP_CONFIG_URL = 'https://app.contentful.com/deeplink?link=apps';
+
+/*
+* The error messages are split on `$` and then again on `|`. The `$` is used to
+* parse the message's "link" url. The`|` is used to parse the message's "link"
+* title. If the `message` string has no `|` then the `$` is used to parse the
+* message's "link" url and title.
+
+* E.g.
+* `Return to $${APP_CONFIG_URL}|App Config Page$ to enter a valid API key`
+* Will be parsed as: 
+* `Return to https://app.contentful.com/deeplink?link=apps to enter a valid
+* API key`.
+*/
+
+const ERROR_MESSAGES = {
+  InvalidApiKeyError: {
+    message: `Return to $${APP_CONFIG_URL}|App Config Page$ to enter a valid API key`,
+    name: 'Invalid API Key',
+    type: 'negative',
+  },
+  NoSourcesError: {
+    message: `Go to $${DASHBOARD_URL}$ to add an imgix source.`,
+    name: 'You have no imgix sources',
+    type: 'warning',
+  },
+  NoOriginImagesError: {
+    message: `Go to $${DASHBOARD_URL}$ to add an imgix source.`,
+    name: 'This Source has no Origin images',
+    type: 'warning',
+  },
+} as const;
+
+type ErrorMessageType = keyof typeof ERROR_MESSAGES;
+
+/**
+ * @class IxError
+ * @description
+ * This class is used to manage imgix API error messages and warnings. It
+ * extends the builtin `Error` class and adds the `type` property.
+ *
+ * @param {NoteType} type "`InvalidApiKeyError` | `NoSourcesError` | `NoOriginImagesError`"
+ * @param {string} message The error message string.
+ *
+ * @example
+ * const errors = [new IxError()];
+ * console.log(errors[0].name);
+ * // => 'imgix API Error'
+ * console.log(errors[0].message);
+ * // => 'Boom 💥'
+ * console.log(errors[0].type);
+ * // => 'negative'
+ */
+
+export class IxError extends Error {
+  type: NoteType;
+  constructor(type: ErrorType, message?: string) {
+    super(message);
+    this.name =
+      ERROR_MESSAGES[type as ErrorMessageType].name || 'imgix API Error';
+    this.message = message || ERROR_MESSAGES[type as ErrorMessageType].message;
+    this.type = ERROR_MESSAGES[type as ErrorMessageType].type || 'warning';
+  }
+}
+
+export const invalidApiKeyError = (message?: string) =>
+  new IxError('InvalidApiKeyError', message);
+
+export const noSourcesError = (message?: string) =>
+  new IxError('NoSourcesError', message);
+
+export const noOriginImagesError = (message?: string) =>
+  new IxError('NoOriginImagesError', message);
