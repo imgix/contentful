250da50c45d8d54446863ffd913fe4e43114cae7
sherwinski <sherwin@imgix.com>
2021-08-11 13:05:37 -0400

    chore: remove any unused imports or imports referencing removed code

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,9 @@
-import React from 'react';
 import { render } from 'react-dom';
 
 import {
   AppExtensionSDK,
   FieldExtensionSDK,
-  SidebarExtensionSDK,
   DialogExtensionSDK,
-  EditorExtensionSDK,
-  PageExtensionSDK,
   init,
   locations,
 } from 'contentful-ui-extensions-sdk';
@@ -16,9 +12,6 @@ import '@contentful/forma-36-fcss/dist/styles.css';
 import '@contentful/forma-36-tokens/dist/css/index.css';
 
 import Config from './components/ConfigScreen';
-import EntryEditor from './components/EntryEditor/';
-import Page from './components/Page/';
-import Sidebar from './components/Sidebar/';
 import Field from './components/Field/';
 import Dialog from './components/Dialog/';
 
@@ -47,20 +40,8 @@ if (process.env.NODE_ENV === 'development' && window.self === window.top) {
         component: <Field sdk={sdk as FieldExtensionSDK} />,
       },
       {
-        location: locations.LOCATION_ENTRY_EDITOR,
-        component: <EntryEditor sdk={sdk as EditorExtensionSDK} />,
-      },
-      {
         location: locations.LOCATION_DIALOG,
         component: <Dialog sdk={sdk as DialogExtensionSDK} />,
-      },
-      {
-        location: locations.LOCATION_ENTRY_SIDEBAR,
-        component: <Sidebar sdk={sdk as SidebarExtensionSDK} />,
-      },
-      {
-        location: locations.LOCATION_PAGE,
-        component: <Page sdk={sdk as PageExtensionSDK} />,
       },
     ];
 
