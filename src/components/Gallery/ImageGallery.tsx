21d3c085f094e11edb6aabdee3da38959c0414ce
sherwinski <sherwin@imgix.com>
2021-10-27 15:29:04 -0700

    refactor: pull up asset parsing from ImageGallery to Dialog component
    In order to implement search, we will need the search UI (text input and button) to have access to the same logic where assets are requested and parsed. Unfortunately there did not appear to be a very react-idiomatic way to handle this situation while also keeping the Dialog and ImageGallery components fully separated.
    The current solution here is to pull up all asset-related logic to the Dialog level. Once all processing is done, that data can be passed down to ImageGallery as a prrop for rendering

--- src/components/Gallery/ImageGallery.tsx
+++ src/components/Gallery/ImageGallery.tsx
@@ -15,6 +15,7 @@ interface GalleryProps {
   getTotalImageCount: (totalImageCount: number) => void;
   pageInfo: PageProps;
   changePage: (newPageIndex: number) => void;
+  assets: Array<string>;
 }
 
 interface GalleryState {
@@ -32,100 +33,6 @@ export class Gallery extends Component<GalleryProps, GalleryState> {
     };
   }
 
-  getImages = async () => {
-    const assets = await this.props.imgix.request(
-      `assets/${this.props.selectedSource?.id}?page[number]=${this.props.pageInfo.currentIndex}&page[size]=18`,
-    );
-    // TODO: add more explicit types for image
-    this.props.getTotalImageCount(
-      parseInt((assets.meta.cursor as any).totalRecords || 0),
-    );
-    return assets;
-  };
-
-  getImagePaths = async () => {
-    let images,
-      allOriginPaths: string[] = [];
-
-    try {
-      images = await this.getImages();
-    } catch (error) {
-      // APIError will emit more helpful data for debugging
-      if (error instanceof APIError) {
-        console.error(error.toString());
-      } else {
-        console.error(error);
-      }
-      return allOriginPaths;
-    }
-
-    /*
-     * Resolved requests can either return an array of objects or a single
-     * object via the `data` top-level field. When parsing all enabled sources,
-     * both possibilities must be accounted for.
-     */
-    if (images) {
-      const imagesArray = Array.isArray(images.data)
-        ? images.data
-        : [images.data];
-      imagesArray.map((image: any) =>
-        // TODO: add more explicit types for image
-        allOriginPaths.push(image.attributes.origin_path),
-      );
-
-      return allOriginPaths;
-    } else {
-      return [];
-    }
-  };
-
-  /*
-   * Constructs an array of imgix image URL from the selected source in the
-   * application Dialog component
-   */
-  constructUrl(images: string[]) {
-    const scheme = 'https://';
-    const domain = this.props.selectedSource.name;
-    const imgixDomain = '.imgix.net';
-
-    const urls = images.map(
-      (path: string) => scheme + domain + imgixDomain + path,
-    );
-    return urls;
-  }
-
-  /*
-   * Requests and constructs fully-qualified image URLs, saving the results to
-   * state
-   */
-  async requestImageUrls() {
-    // if selected source, return images
-    if (Object.keys(this.props.selectedSource).length) {
-      const images = await this.getImagePaths();
-      const fullUrls = this.constructUrl(images);
-      // if at least one path, remove placeholders
-
-      if (fullUrls.length) {
-        this.setState({ fullUrls });
-      } else {
-        this.setState({ fullUrls: [] });
-      }
-    }
-  }
-
-  async componentDidMount() {
-    this.requestImageUrls();
-  }
-
-  async componentDidUpdate(prevProps: GalleryProps) {
-    if (
-      this.props.selectedSource.id !== prevProps.selectedSource.id ||
-      this.props.pageInfo.currentIndex !== prevProps.pageInfo.currentIndex
-    ) {
-      this.requestImageUrls();
-    }
-  }
-
   handleClick = (selectedImage: string) => this.setState({ selectedImage });
 
   handleSubmit = () => {
@@ -135,14 +42,14 @@ export class Gallery extends Component<GalleryProps, GalleryState> {
   render() {
     const { fullUrls, selectedImage } = this.state;
 
-    if (!fullUrls.length) {
+    if (!this.props.assets.length) {
       return <ImagePlaceholder />;
     }
 
     return (
       <div>
         <div className="ix-gallery">
-          {fullUrls.map((url: string) => {
+          {this.props.assets.map((url: string) => {
             return (
               <GridImage
                 key={url}
@@ -160,7 +67,7 @@ export class Gallery extends Component<GalleryProps, GalleryState> {
             changePage={this.props.changePage}
           />
           <ImageSelectButton
-            hidden={!!fullUrls.length}
+            hidden={!!this.props.assets.length}
             disabled={selectedImage === ''}
             handleSubmit={this.handleSubmit}
           />
