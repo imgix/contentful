1b6b64dcb9623aecf1001a83b320c505f7a63401
sherwinski <sherwin@imgix.com>
2021-12-21 13:43:48 -0800

    chore: export `AssetProps` definition from Dialog
    Co-authored-by: Frederick Fogerty <frederick@imgix.com>

--- src/components/Dialog/index.ts
+++ src/components/Dialog/index.ts
@@ -1,7 +1,8 @@
-import Dialog, { SourceProps as _SourceProps, PageProps as _PageProps } from './Dialog';
+import Dialog, { SourceProps as _SourceProps, PageProps as _PageProps, AssetProps as _AssetProps } from './Dialog';
 import { DialogHeader as _DialogHeader } from './DialogHeader';
 
 export default Dialog;
 export const DialogHeader = _DialogHeader;
 export type SourceProps = _SourceProps;
 export type PageProps = _PageProps;
+export type AssetProps = _AssetProps;
