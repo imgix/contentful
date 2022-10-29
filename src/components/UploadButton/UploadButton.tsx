bcb9be1657e4e6ceabbb43794d0a6817b7e87e0c
Luis Ball <lball@imgix.com>
2022-10-28 23:33:43 -0700

    feat: create upload button
    This commit adds an `<UploadButton />` component. This component
    triggers the OS filepicker dialog and stores the selected file in an
    object inside a callback function. This object can be used in a later
    commit to upload the file to AM.

---
+++ src/components/UploadButton/UploadButton.tsx
@@ -0,0 +1,48 @@
+import { useRef } from 'react';
+
+import { Button } from '@contentful/forma-36-react-components';
+import './UploadButton.css';
+
+export function UploadButton(props: { handleClick: (args?: any) => void }) {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const handleClick = () => {
+    // open file input box
+    inputRef?.current?.click();
+  };
+
+  const handleFileChange = (event: any) => {
+    const inputEle = event?.target as HTMLInputElement;
+    // store the fileObject for later use
+    const fileObj = inputEle.files && inputEle.files[0];
+    if (!fileObj) {
+      return;
+    }
+
+    // TODO: trigger a file editor modal
+    // and pass the modal the file object
+
+    // reset the input field value
+    inputEle.value = '';
+  };
+
+  return (
+    <div>
+      <Button
+        buttonType="positive"
+        className="ix-uploadButton"
+        icon="Download"
+        type="submit"
+        onClick={handleClick}
+      >
+        Upload
+      </Button>
+      <input
+        id="ix-file-upload"
+        style={{ display: 'none' }}
+        ref={inputRef}
+        type="file"
+        onChange={handleFileChange}
+      />
+    </div>
+  );
+}
