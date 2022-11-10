9f1200fafa0eb9a1d1a0db3e55270c0e0f8d046f
Alice Lawrie <Alice.lawrie@imgix.com>
2022-11-10 14:06:47 -0500

    feat: add icon component for SVG support

---
+++ src/components/Icons/Icon.tsx
@@ -0,0 +1,8 @@
+import React, { ReactChild } from "react";
+import "./Icon.css"
+
+export const Icon = ({ className, children }: { className?: string, children:ReactChild }) => (
+  <div className={`container ${className ? className : ""}`}>
+    {children}
+  </div>
+);
