c807cca300b9b6a75b0f8fe8cdcb906a8a46767c
Alice Lawrie <Alice.lawrie@imgix.com>
2022-11-10 14:07:21 -0500

    chore(icon): add svg icons

---
+++ src/components/Icons/VideoAssetSVG.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Icon } from './Icon';
+
+export const VideoAssetSVG = () => {
+  return (
+    <Icon>
+      <svg
+        version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlnsXlink="http://www.w3.org/1999/xlink"
+        width="60%"
+        height="60%"
+        viewBox="0 0 24 24"
+      >
+        <path
+          fill="#364C5B"
+          d="M21,6.4c0-0.3-0.1-0.5-0.3-0.7l-5.4-5.4C15.1,0.1,14.9,0,14.6,0v0 C14.6,0,4,0,4,0C3.4,0,3,0.4,3,1v22c0,0.6,0.4,1,1,1h16c0.6,0,1-0.4,1-1C21,23,21,6.4,21,6.4L21,6.4z"
+        />
+        <path
+          fill="#7D90A0"
+          d="M14.6,5.4c0,0.6,0.4,1,1,1l5.4,0c0-0.3-0.1-0.5-0.3-0.7l-5.4-5.4 C15.1,0.1,14.9,0,14.6,0L14.6,5.4z"
+        />
+        <polygon fill="#7D90A0" points="16,15 8,20 8,10 " />
+      </svg>
+    </Icon>
+  );
+};
