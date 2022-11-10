30c1486fda2c1eeba6b9c28377190ab25e83711b
Alice Lawrie <Alice.lawrie@imgix.com>
2022-11-10 14:07:21 -0500

    chore(icon): add svg icons

---
+++ src/components/Icons/AiAssetSVG.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Icon } from './Icon';
+
+export const AiAssetSVG = () => {
+  return (
+    <Icon>
+      <svg
+        version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlnsXlink="http://www.w3.org/1999/xlink"
+        width="40%"
+        height="40%"
+        viewBox="0 0 24 24"
+      >
+        <path
+          fill="#EEA620"
+          d="M21,6.4c0-0.3-0.1-0.5-0.3-0.7l-5.4-5.4C15.1,0.1,14.9,0,14.6,0v0 C14.6,0,4,0,4,0C3.4,0,3,0.4,3,1v22c0,0.6,0.4,1,1,1h16c0.6,0,1-0.4,1-1C21,23,21,6.4,21,6.4L21,6.4z"
+        />
+        <path
+          fill="#FBED96"
+          d="M14.6,5.4c0,0.6,0.4,1,1,1l5.4,0c0-0.3-0.1-0.5-0.3-0.7l-5.4-5.4 C15.1,0.1,14.9,0,14.6,0L14.6,5.4z"
+        />
+        <path
+          fill="#FBED96"
+          d="M15.7,12.4h-1.4v-1.1h1.4V12.4z M15.7,18.9h-1.4v-5.3h1.4V18.9z"
+        />
+        <path
+          fill="#FBED96"
+          d="M12.1,17.4h-1.8L10,18.9H8.6l1.9-7.1H12l1.9,7.1h-1.4L12.1,17.4z M10.6,16.3h1.2l-0.6-2.7h0L10.6,16.3z"
+        />
+      </svg>
+    </Icon>
+  );
+};
