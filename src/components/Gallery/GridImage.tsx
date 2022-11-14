062ce84a281df587e59b3af537a48d7d468f8218
Alice Lawrie <Alice.lawrie@imgix.com>
2022-11-14 08:10:53 -0500

    chore: add svg, text detection for icons

--- src/components/Gallery/GridImage.tsx
+++ src/components/Gallery/GridImage.tsx
@@ -1,13 +1,14 @@
 import { FunctionComponent, MouseEventHandler, useState } from 'react';
 import Imgix from 'react-imgix';
 import { AssetProps } from '../Dialog';
-import { AiAssetSVG } from '../Icons/AiAssetSVG';
 import { PDFAssetSVG } from '../Icons/PDFAssetSVG';
 import { UnknownAssetSVG } from '../Icons/UnknownAssetSVG';
 import { VideoAssetSVG } from '../Icons/VideoAssetSVG';
 import { ImageAssetSVG } from '../Icons/ImageAssetSVG';
 
 import './GridImage.css';
+import { DocumentAssetSVG } from '../Icons/DocumentAssetSVG';
+import { SVGAssetSVG } from '../Icons/SVGAssetSVG';
 
 export type GridImageComponentProps = {
   asset: AssetProps;
@@ -54,10 +55,14 @@ export const GridImage: FunctionComponent<GridImageComponentProps> = ({
       <div className={'ix-gallery-image-gradient' + focus}></div>
       {!asset.attributes.content_type ? (
         <UnknownAssetSVG />
+      ) : asset.attributes.content_type.startsWith('image/svg') ? (
+        <SVGAssetSVG />
       ) : asset.attributes.content_type.startsWith('image') ? (
         <GridImageAsset />
       ) : asset.attributes.content_type.startsWith('video') ? (
         <VideoAssetSVG />
+      ) : asset.attributes.content_type.startsWith('text') ? (
+        <DocumentAssetSVG />
       ) : asset.attributes.content_type === 'application/pdf' ? (
         <PDFAssetSVG />
       ) : (
