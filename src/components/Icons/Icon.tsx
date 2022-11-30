87912bc64f96a9a8fe64214f00cd535e04654dfe
sherwinski <sherwin@imgix.com>
2022-11-30 15:48:07 -0800

    style: sync with prettier

--- src/components/Icons/Icon.tsx
+++ src/components/Icons/Icon.tsx
@@ -1,8 +1,12 @@
-import React, { ReactChild } from "react";
-import "./Icon.css"
+import React, { ReactChild } from 'react';
+import './Icon.css';
 
-export const Icon = ({ className, children }: { className?: string, children:ReactChild }) => (
-  <div className={`container ${className ? className : ""}`}>
-    {children}
-  </div>
+export const Icon = ({
+  className,
+  children,
+}: {
+  className?: string;
+  children: ReactChild;
+}) => (
+  <div className={`container ${className ? className : ''}`}>{children}</div>
 );
