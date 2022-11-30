73163c8342104ae4d90d0b0960924f7bb2eafc02
Luis Ball <lball@imgix.com>
2022-11-30 10:17:16 -0500

    feat: create gallery placeholder component
    This commit creates a re-usabl placeholder component that accepts a
    `text` prop. The component incorporates the action bar to allow the
    users to close the modal even when viewing a placeholder.

---
+++ src/components/Gallery/GalleryPlaceholder.tsx
@@ -0,0 +1,19 @@
+import { ReactElement } from 'react';
+import { Paragraph } from '@contentful/forma-36-react-components';
+import { ActionBar } from '../ActionBar';
+import './ImagePlaceholder.css';
+
+export function GalleryPlaceholder({
+  text,
+  sdk,
+}: {
+  text: string;
+  sdk: any;
+}): ReactElement {
+  return (
+    <div className="ix-grid-item-placeholder">
+      <Paragraph className="ix-placeholder-text">{text}</Paragraph>
+      <ActionBar handleClose={sdk.close} />
+    </div>
+  );
+}
