50d864f88cf619d0217b5b192f831fb74c25b86a
Luis Ball <lball@imgix.com>
2022-11-30 10:21:44 -0500

    refactor: use gallery-placeholder for placeholders
    Before this commit, each placholder was it's own component. After this
    commit, the `<GalleryPlaceholder />` component handlees the rendering
    for the placeholders, accepting the `text` prop for the message.

--- src/components/Gallery/index.ts
+++ src/components/Gallery/index.ts
@@ -1,13 +1,7 @@
 import { Gallery as _ImageGallery } from './ImageGallery';
 import { GridImage as _GridImage } from './GridImage';
-import { SourcePlaceholder as _SourcePlaceholder } from './SourcePlaceholder';
-import { EmptySourcePlaceholder as _EmptySourcePlaceholder } from './EmptySourcePlaceholder';
-import { WebFolderPlaceholder as _WebFolderPlaceholder } from './WebFolderPlaceholder';
 import { ImagePagination as _ImagePagination } from './ImagePagination';
-
+export { GalleryPlaceholder } from './GalleryPlaceholder';
 export const ImageGallery = _ImageGallery;
 export const GridImage = _GridImage;
-export const EmptySourcePlaceholder = _EmptySourcePlaceholder;
-export const SourcePlaceholder = _SourcePlaceholder;
-export const WebFolderPlaceholder = _WebFolderPlaceholder;
 export const ImagePagination = _ImagePagination;
